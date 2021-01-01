4c6827335e67253b62d38e31fc68273e7ac9f6c8
Ian Turner <ianturner88@gmail.com>
2021-01-01 10:44:37 -0500

    useState hook

--- hooks-intro/src/App.js
+++ hooks-intro/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function decrementCount() {
+    setCount((previousCount) => previousCount - 1);
+  }
+
+  function updateCount() {
+    setCount((previousCount) => previousCount + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={updateCount}>+</button>
+    </>
   );
 }
 
