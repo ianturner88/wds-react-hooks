c80ac67cd2dab0a05673547762e1f3a8d3106ae1
Ian Turner <ianturner88@gmail.com>
2021-01-03 13:33:51 -0500

    useRef boilerplate code

--- hooks-intro/src/App.js
+++ hooks-intro/src/App.js
@@ -3,11 +3,12 @@ import Hook1 from './hooks/Hook1-useState';
 import Hook2 from './hooks/Hook2-useEffect';
 import Hook3 from './hooks/Hook3-useEffect';
 import Hook4 from './hooks/Hooks4-useMemo';
+import Hook5 from './hooks/Hooks5-useRef';
 
 function App() {
   return (
     <>
-      <Hook4 />
+      <Hook5 />
     </>
   );
 }
