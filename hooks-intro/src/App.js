2fa57ac5d423744ebb767432f6e14efa162885ac
Ian Turner <ianturner88@gmail.com>
2021-01-03 10:38:02 -0500

    useMemo boilerplate code

--- hooks-intro/src/App.js
+++ hooks-intro/src/App.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import Hook1 from './hooks/Hook1-useState';
 import Hook2 from './hooks/Hook2-useEffect';
 import Hook3 from './hooks/Hook3-useEffect';
+import Hook4 from './hooks/Hooks4-useMemo';
 
 function App() {
   return (
     <>
-      <Hook3 />
+      <Hook4 />
     </>
   );
 }
---
+++ hooks-intro/src/hooks/Hooks4-useMemo.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+function Hook4() {
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  const doubleNumber = slowFunction(number);
+  const themeStyles = {
+    backgroundColor: dark ? 'black' : 'white',
+    color: dark ? 'white' : 'black',
+  };
+
+  return (
+    <>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change Theme
+      </button>
+      <div style={themeStyles}>{doubleNumber}</div>
+    </>
+  );
+}
+
+function slowFunction(num) {
+  console.log('Calling Slow Function');
+  for (let i = 0; i <= 1000000000; i++) {}
+  return num * 2;
+}
+
+export default Hook4;
